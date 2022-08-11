194fa94cf0399261b1165d9835d0b83a54430481
Muhammad Zaki ZN <73261827+muhammadzakizn@users.noreply.github.com>
2022-08-11 08:10:14 +0800

    assets

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+        
+    alert("PROGRESS: 59.8% (11.44 26.06.2022)\n\n\nWebsite ini sedang dalam pembangunan, Apabila menemukan kesalahan harap hubungi Penyedia Situs ini. \n\nDengan menekan tombol 'OK' Anda menyetujui 'Syarat dan Ketentuan Layanan' beserta 'Cookies' disitus ini demi kelancaran dan melacak kesalahan. \n\nsites by hostinger (c) 2022");
+
+
+
+});
